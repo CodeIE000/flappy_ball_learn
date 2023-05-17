0190a2fabd499974282a3f622500ff2447c1832f
CodeIE000 <idulsa.emmannicholasblabe.000@gmail.com>
2023-05-17 20:08:18 +0800

    added almost all graphics

--- flappy_ball_tutorial.py
+++ flappy_ball_tutorial.py
@@ -4,16 +4,16 @@ import time
 import os
 import random
 
-WIN_WIDTH = 600
-WIN+HEIGHT = 800
+WIN_WIDTH = 500
+WIN_HEIGHT = 670
 
-BIRD_IMGS = [pygame.image.load(os.path.join("imgs", "ball1.png")), pygame.image.load(os.path.join("imgs", "ball2.png")), pygame.image.load(os.path.join("imgs", "ball3.png"))]
+BALL_IMGS = [pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "ball1.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "ball2.png"))), pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "ball3.png")))]
 PIPE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "pipe.png")))
 BASE_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "base.png")))
 BG_IMG = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "bg.png")))
 
-class Bird:
-	IMGS = BIRD_IMGS
+class Ball:
+	IMGS = BALL_IMGS
 	MAX_ROTATION = 25
 	ROT_VEL = 20
 	ANIMATION_TIME = 5
@@ -38,6 +38,146 @@ class Bird:
 
 		d = self.vel*self.tick_count + 1.5*self.tick_count**2
 
-		# uep
-		# hello
+		if d >= 16:
+			d = 16
 
+		if d < 0:
+			d -= 20
+
+		self.y = self.y + d
+
+		if d < 0 or self.y < self.height + 50:
+			if self.tilt < self.MAX_ROTATION:
+				self.tilt = self.MAX_ROTATION
+		else:
+			if self.tilt > -90:
+				self.tilt -= self.ROT_VEL
+
+	def draw(self, win):
+		self.img_count += 1
+
+		if self.img_count < self.ANIMATION_TIME:                                                                            
+			self.img = self.IMGS[0]
+		elif self.img_count < self.ANIMATION_TIME*2:
+			self.img = self.IMGS[1]
+		elif self.img_count < self.ANIMATION_TIME*3:
+			self.img = self.IMGS[2]
+		elif self.img_count < self.ANIMATION_TIME*4:
+			self.img = self.IMGS[1]
+		elif self.img_count == self.ANIMATION_TIME*4 + 1:
+			self.img = self.IMGS[0]
+			self.img_count = 0
+
+		if self.tilt <= -80:
+			self.img = self.IMGS[1]
+			self.imgcount = self.ANIMATION_TIME*2
+
+		rotated_image = pygame.transform.rotate(self.img, self.tilt)
+		new_rect = rotated_image.get_rect(center=self.img.get_rect(topleft = (self.x, self.y)).center)
+		win.blit(rotated_image, new_rect.topleft) 
+
+	def get_mask(self):
+		return pygame.mask.from_surface(self.img)
+
+class Pipe:
+	GAP = 200
+	VEL = 5
+
+	def __init__(self, x):
+		self.x = x
+		self.height = 0
+
+		self.top = 0
+		self.bottom = 0
+		self.PIPE_TOP = pygame.transform.flip(PIPE_IMG, False, True)
+		self.PIPE_BOTTOM = PIPE_IMG
+
+		self.passed = False
+		self.set_height()
+
+	def set_height(self):
+		self.height = random.randrange(50, 450)
+		self.top = self.height - self.PIPE_TOP.get_height()
+		self.bottom = self.height + self.GAP
+
+	def move(self):
+		self.x -= self.VEL
+
+	def draw(self, win):
+		win.blit(self.PIPE_TOP, (self.x, self. top))
+		win.blit(self.PIPE_BOTTOM, (self.x, self.bottom))
+
+	def collide(self, ball):
+		ball_mask = bird.get_mask()
+		top_mask = pygame.mask.from_surface(self.PIPE_TOP)
+		bottom_mask = pygame.mask.from_surface(self.PIPE_BOTTOM)
+
+		top_offset = (self.x - ball.x, self.top - round(ball.y))
+		bottom_offset = (self.x - bird.x, self.bottom - round(ball.y))
+
+		b_point = ball_mask.overlap(bottom_mask, bottom_offset)
+		t_point = ball_mask.overlap(top_mask, top_offset)
+
+		if t_point or b_point:
+			return True
+
+		return False
+
+class Base:
+	VEL = 5
+	WIDTH = BASE_IMG.get_width()
+	IMG = BASE_IMG
+
+	def __init__(self, y):
+		self.y = y
+		self.x1 = 0
+		self.x2 = self.WIDTH
+
+	def move(self):
+		self.x1 -= self.VEL
+		self.x2 -= self.VEL
+
+		if self.x1 + self.WIDTH < 0:
+			self.x1 = self.x2 + self.WIDTH
+
+		if self.x2 + self.WIDTH < 0:
+			self.x2 = self.x1 + self.WIDTH
+
+	def draw(self, win):
+		win.blit(self.IMG, (self.x1, self.y))
+		win.blit(self.IMG, (self.x2, self.y))
+
+
+def draw_window(win, ball, pipes, base):
+	win.blit(BG_IMG, (0,-160))
+
+	for pipe in pipes:
+		pipe.draw(win)
+
+	base.draw(win)
+
+	ball.draw(win)
+	pygame.display.update()
+
+def main():
+	ball = Ball(230,300)
+	base = Base(600)
+	pipes = [Pipe(700)]
+	win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+	clock = pygame.time.Clock()
+
+	run = True
+	while run:
+		clock.tick(30)
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+		#ball.move()
+		base.move()
+		draw_window(win, ball, pipes, base)
+
+	pygame.quit()
+	quit()
+
+main()
